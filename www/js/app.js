d1510aa900edae8d9aab49713eb35a4ec3f31cc3
Alfonso Pablov <alfonso.pablov@outlook.com>
2015-03-06 23:11:16 +0800

    2015-30-06 - Created pages for invoices and estimates list.(List, edit, delete, add feature) - Added some features for Clients.(List, edit, delete, add feature)

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,12 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('myApp', ['ionic', 
+angular.module('myApp', [
+  'ionic',
   'myApp.constants',
   'myApp.filters',
   'myApp.directives',
   'myApp.services',
   'myApp.controllers'
-])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+]).run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -23,5 +16,6 @@ angular.module('myApp', ['ionic',
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    
   });
 });
